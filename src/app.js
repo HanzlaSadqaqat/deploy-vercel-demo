1fb226296de8cb2e8e5ed9cc684f4e016eb6dadf
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2024-02-22 17:04:32 +0000

    deploy

--- src/app.js
+++ src/app.js
@@ -26,4 +26,11 @@ app.use('/api/v1', api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
+
 module.exports = app;
